04fae7e46bf328bf1e8825db3f0b173f1534a673
Bram Adl <hello.bramadl@gmail.com>
2026-07-04 05:24:26 +0700

    build(next): configure allowedDevOrigins option for ngrok origin

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+	/* config options here */
+	allowedDevOrigins: process.env.NGROK_FREE_TUNNEL
+		? [process.env.NGROK_FREE_TUNNEL]
+		: undefined,
+	reactCompiler: true,
 };
 
 export default nextConfig;
