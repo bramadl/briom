b4bf929f04b2c6fe41c239ea51b88ecf659d42ff
Bram Adl <hello.bramadl@gmail.com>
2026-07-03 19:34:24 +0700

    build: re-configure drizzle, preparing new migrations and schemas

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,17 +1,21 @@
-import type { Config } from "drizzle-kit";
+import dotenv from "dotenv";
+import { defineConfig } from "drizzle-kit";
 
-const connectionUrl = process.env.DATABASE_URL;
-if (!connectionUrl) throw new Error("DATABASE_URL is required");
+/**
+ * @description
+ * Load local env in local development only.
+ * Vercel adds VERCEL env by default that we can use to check
+ * against. Vercel also resolves env vars automatically so
+ * this code should never break (just for an extra-defense).
+ */
+if (!process.env.VERCEL) dotenv.config({ path: ".env.local" });
 
-export default {
-	schema: "./src/libs/providers/drizzle/schema.ts",
-	out: "./src/libs/providers/drizzle/migrations",
+const databaseUrl = process.env.DATABASE_URL;
+if (!databaseUrl) throw new Error("DATABASE_URL is required");
+
+export default defineConfig({
+	out: "./src/core/infrastructure/.providers/drizzle/migrations",
+	schema: "./src/core/infrastructure/.providers/drizzle/schema.ts",
 	dialect: "postgresql",
-	dbCredentials: { url: connectionUrl },
-	migrations: {
-		prefix: "index",
-		table: "__drizzle_migrations__",
-		schema: "public",
-	},
-	strict: false,
-} satisfies Config;
+	dbCredentials: { url: databaseUrl },
+});
