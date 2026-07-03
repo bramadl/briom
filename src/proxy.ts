c166651441b49fe407ba302c7f9b8a1686b52147
Bram Adl <hello.bramadl@gmail.com>
2026-07-03 19:32:21 +0700

    fix(proxy|libs): use supabase session middleware in proxy and remove server action utils–will rewrite it later

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,42 +1,9 @@
-import { createServerClient } from "@supabase/ssr";
-import { type NextRequest, NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-import { getSupabaseServerEnvs } from "./libs/get-env";
+import { supabaseSessionMiddleware } from "./core/infrastructure/auth";
 
 export async function proxy(request: NextRequest) {
-	const { supabasePublishableKey, supabaseUrl } = getSupabaseServerEnvs();
-
-	let response = NextResponse.next({ request });
-	const supabase = createServerClient(supabaseUrl, supabasePublishableKey, {
-		cookies: {
-			getAll() {
-				return request.cookies.getAll();
-			},
-			setAll(cookiesToSet) {
-				for (const { name, value } of cookiesToSet) {
-					request.cookies.set(name, value);
-				}
-
-				response = NextResponse.next({ request });
-				for (const { name, value, options } of cookiesToSet) {
-					response.cookies.set(name, value, options);
-				}
-			},
-		},
-	});
-
-	const { pathname } = request.nextUrl;
-	const {
-		data: { user },
-	} = await supabase.auth.getUser();
-
-	if (!user && pathname.startsWith("/rooms")) {
-		return NextResponse.redirect(new URL("/", request.url));
-	} else if (user && pathname === "/") {
-		return NextResponse.redirect(new URL("/rooms", request.url));
-	}
-
-	return response;
+	return await supabaseSessionMiddleware(request, { protectedRoute: "/rooms" });
 }
 
 export const config = {
