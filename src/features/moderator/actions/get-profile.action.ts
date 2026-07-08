d63adcd7508c036c8fe5e9a35a9577f4b451083e
Bram Adl <hello.bramadl@gmail.com>
2026-07-08 12:09:23 +0700

    fix(fe): race condition in auth's moderator webhook–make a direct enqueue in getProfile which registers or get the user if exists | opting out webhook on the next deployment

--- src/features/moderator/actions/get-profile.action.ts
+++ src/features/moderator/actions/get-profile.action.ts
@@ -6,6 +6,25 @@ import { respond } from "@briom/libs/server-action";
 
 export async function getProfile() {
 	const user = await getUser();
-	const result = await briom.moderators.profile({ moderatorId: user.id });
+	let result = await briom.moderators.profile({ moderatorId: user.id });
+
+	if (!result.value().moderator) {
+		const registerResult = await briom.moderators.register({
+			id: user.id,
+			email: user.email as string,
+			name: user.user_metadata?.name ?? user.email,
+			avatar: user.user_metadata?.avatar_url ?? null,
+		});
+
+		if (
+			registerResult.isError() &&
+			registerResult.error().code !== "EMAIL_ALREADY_USED"
+		) {
+			return respond(registerResult);
+		}
+
+		result = await briom.moderators.profile({ moderatorId: user.id });
+	}
+
 	return respond(result);
 }
